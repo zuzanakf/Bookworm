b0bec12bff0a3b835dee4048645363aa3c0d1f84
Zuzana Fernandes <zuzana.fernandes@yahoo.co.uk>
2025-03-19 11:43:58 +0000

    swift data starter intro

---
+++ Bookworm/Student.swift
@@ -0,0 +1,21 @@
+//
+//  Student.swift
+//  Bookworm
+//
+//  Created by Zuzana.Fernandes on 19/03/2025.
+//
+// swift data learning
+import Foundation
+import SwiftData
+
+// same as @ observable for for swift data instead
+@Model
+class Student {
+    var id: UUID
+    var name: String
+    
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
